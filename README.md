e994e776aacb5e4d411577f6d4ca011fa23ca263
Aruna G <Arunagovindaraju@yahoo.com>
2018-08-17 01:08:19 -0400

    Included GPARS and parallel programming

--- README.md
+++ README.md
@@ -9,6 +9,8 @@ JSONSlurper & File parser
 Closure & Trampoline for Recursive Inner Class
 GPARS library & Parallel  
 List Operations for Collect, Flatten, Add & Minus
+Less Verbose and easy append free print statements
+In line assert statements
 
 #Feature Not used
 recursive memorize is removed since a visited link is ignored.
--- src/com/ge/crawler/JSONCrawler.groovy
+++ src/com/ge/crawler/JSONCrawler.groovy
@@ -1,21 +1,17 @@
 package com.ge.crawler
 
-import java.util.Collection
-import java.util.List
-import groovyx.gpars.*
-
 import groovy.json.JsonSlurper
+import groovyx.gpars.GParsPool
 
 /** 
- * Class responsible for finding and storing the valid links 
- * from JSON files.
+ * Class same as JSONCrawler
+ * This class is an example that uses GPARS.jar for Asynchronous Parallel Collection navigation 
  * 
  * @author arunag
  *
  */
 class JSONCrawler {
-	static String HOME="http://foo.bar.com/p1"
-
+	
 	static String FILE1 = "files/Internet1.json";
 	static String FILE2 = "files/Internet2.json";
 	static String FILE3 = "files/Internet3.json";
@@ -23,6 +19,7 @@ class JSONCrawler {
 	static String PAGES = "pages"
 	static String ADDRESS = "address"
 	static String LINKS = "links"
+	static String HOME="http://foo.bar.com/p1"
 	
 	/** 
 	 * Main Class that feeds three different input files to 
@@ -31,43 +28,42 @@ class JSONCrawler {
 	 */
 	public static void main (String [] s) {
 		
-		def jsonSlurper = new JsonSlurper()
-		File file = new File(JSONCrawler.FILE1)
-		def json = jsonSlurper.parseFile(file, JSONCrawler.DEFAULT_ENCODING)
-		List pages = json[JSONCrawler.PAGES].collect()
-		JSONCrawler.buildOutputLinks(pages)
-
-		println "\n"
 		
-		jsonSlurper = new JsonSlurper()
-		file = new File(JSONCrawler.FILE2)
-		json = jsonSlurper.parseFile(file, JSONCrawler.DEFAULT_ENCODING)
-		pages = json[JSONCrawler.PAGES].collect()
-		JSONCrawler.buildOutputLinks(pages)
-
-		println "\n"
+		Collection files = [JSONCrawler.FILE1, JSONCrawler.FILE2, JSONCrawler.FILE3]
 		
-		jsonSlurper = new JsonSlurper()
-		file = new File(JSONCrawler.FILE3)
-		json = jsonSlurper.parseFile(file, JSONCrawler.DEFAULT_ENCODING)
-		pages = json[JSONCrawler.PAGES].collect()
-		JSONCrawler.buildOutputLinks(pages)
-
+//		Runs the files in parallel using the GPARS component.
+		GParsPool.withPool(3) {
+			files.eachParallel{
+				println it
+				JSONCrawler.crawl(it)
+			}
+		}
 	}
 	
 	/*
-	 *
+	 * Builds the required Output links such as the "successUrls", skippedUrls, errorUrls 
+	 * by iterating through the elements in the Json file.
+	 * 
 	 * @param pages
 	 */
-	static void buildOutputLinks(Collection pages) {
+	private static void crawl(String filePath) {
+		
+		def jsonSlurper = new JsonSlurper()
+		File file = new File(filePath)
+		def json = jsonSlurper.parseFile(file, JSONCrawler.DEFAULT_ENCODING)
+		assert json instanceof Map
+		List pages = json[JSONCrawler.PAGES].collect()
 		
 		List allLinks = []
-		List success = []
-		Set skipped = []
+		List successUrls = []
+		Set skippedUrls = []
 		Set errorUrls = []
 
 		def parents=pages[JSONCrawler.ADDRESS].collect()
 		def children=pages[JSONCrawler.LINKS].collect()
+		
+		assert parents instanceof Collection
+		assert children instanceof Collection
 		
 		allLinks.addAll(parents)
 		allLinks.addAll(children.flatten())
@@ -78,41 +74,65 @@ class JSONCrawler {
 			def startUrlIndex=parents.indexOf(startUrl)
 			def linksToCrawl = children[startUrlIndex].collect().flatten();
 			
-			linksToCrawl.eachWithIndex{ String link, idx ->
-				
-				if (success.contains(link) ) {
-					//return/continue if already visited
-					skipped.add(link)
-					return
+		GParsPool.withPool(3) {
+		linksToCrawl.eachWithIndexParallel{ String link, idx ->
+					
+					if (successUrls.contains(link) ) {
+						//return to next idx if already visited
+						skippedUrls.add(link)
+						return
+					}
+					if (!parents.contains(link)) {
+						/*
+						 * Return to next idx if the link is not a parent , since there are no further links to crawl.
+						 * Note this is as expected. 
+						 * But the parent links must also be indexed but not crawled. This should  be included in "successUrls" list.
+						 */
+						errorUrls.add(link)
+						return
+					}
+	//				linkList.add(link)
+					successUrls.add(link)
+					buildList(link)
 				}
-				if (!parents.contains(link)) {
-					//return/continue if the link is not a parent 
-					errorUrls.add(link)
-					return
-				}
-				
-//				linkList.add(link)
-				success.add(link)
-				buildList(link)
-				
-			}
+		}
 			return allLinks
 		}
 		
 		def startAddress = HOME
-		success.add(startAddress)
+		successUrls.add(startAddress)
 		
 		buildList = buildList.trampoline()
 		buildList(startAddress)
 
 		//Find Urls not index, skipped.
-		List missedLink = JSONCrawler.findMissingUrls(allLinks, success)
+		List missedLink = JSONCrawler.findMissingUrls(allLinks, successUrls)
 		errorUrls.addAll(missedLink)
 
-		println "Success=$success"
-		println "Skipped=$skipped"
-		println "Error=$errorUrls"
+		println "For $filePath :: Success=$successUrls"
+		println "For $filePath :: Skipped=$skippedUrls"
+		println "For $filePath :: Error=$errorUrls"
 		
+		assert !successUrls.equals(null)
+		assert !skippedUrls.equals(null)
+		assert !errorUrls.equals(null)
+		
+		// Inlined test case
+		def validate ={
+			if (filePath.equalsIgnoreCase(JSONCrawler.FILE1)) {
+				assert successUrls.containsAll(INTERNET1_SUCCESS)
+				assert skippedUrls.collect().containsAll(INTERNET1_SKIPPED)
+				assert errorUrls.collect().containsAll(INTERNET1_ERROR)
+				println "Links from $filePath crawled successfully!"
+			} else if (filePath.equalsIgnoreCase(JSONCrawler.FILE2)) {
+				assert successUrls.containsAll(INTERNET2_SUCCESS)
+				assert skippedUrls.collect().containsAll(INTERNET2_SKIPPED)
+				assert errorUrls.collect().containsAll(INTERNET2_ERROR)
+				println "Links from $filePath crawled successfully!"
+			}
+		}
+		validate()
+		 
 	} // end method
 	
 	/**
@@ -122,7 +142,7 @@ class JSONCrawler {
 	 * @param successLinks
 	 * @return
 	 */
-	static List findMissingUrls(List allLinks, List successLinks) {
+	protected static List findMissingUrls(List allLinks, List successLinks) {
 		List diff = [] 
 		if (allLinks.size() > successLinks.size()) {
 			 diff = allLinks.minus(successLinks)
@@ -137,11 +157,36 @@ class JSONCrawler {
 	 * @param parents
 	 * @return
 	 */
-	static boolean isParent(String link, List parents) {
+	protected static boolean isParent(String link, List parents) {
 		boolean isParent = false
 		if (parents.contains(link)) {
 			isParent=true
 		}
 		return true;
 	}
+	
+	
+//	Internet1 crawl results
+	static final INTERNET1_SUCCESS=
+		["http://foo.bar.com/p1", "http://foo.bar.com/p2",
+		"http://foo.bar.com/p4", "http://foo.bar.com/p5",
+		"http://foo.bar.com/p6"]
+	
+	static final INTERNET1_SKIPPED=
+		["http://foo.bar.com/p2",
+		"http://foo.bar.com/p4","http://foo.bar.com/p1",
+		"http://foo.bar.com/p5"]
+	static final INTERNET1_ERROR=
+		["http://foo.bar.com/p3", "http://foo.bar.com/p7"]
+	
+//	Internet2 crawl results	
+	static final INTERNET2_SUCCESS=
+		["http://foo.bar.com/p1", "http://foo.bar.com/p2",
+		"http://foo.bar.com/p3", "http://foo.bar.com/p4",
+		"http://foo.bar.com/p5"]
+	
+	static final INTERNET2_SKIPPED=["http://foo.bar.com/p1"]
+
+		//Object modified to include p6 since this cannot be reached.
+	static final INTERNET2_ERROR=["http://foo.bar.com/p6"]
 }
